2f007b8c4d2664b548bf725d0c7c5b1b73fb6ba0
Eric Salavatcioglu <eric.salavat@gmail.com>
2023-09-17 00:42:03 -0700

    Added tailwind css.

--- webpack.config.js
+++ webpack.config.js
@@ -13,39 +13,39 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env", 
-              "@babel/preset-react"
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    "@babel/preset-env", 
+                    "@babel/preset-react"
+                ]
+            }
+            }
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader'
             ]
-          }
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader', 'postcss-loader'],
         }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      }
-    ]
+      ]
   },
   resolve: {
     alias: {
-      components: path.resolve(__dirname, 'client/src/components'),
+      components: path.resolve(__dirname, 'src/components'),
     },
     extensions: ['', '.js', '.jsx']
   },
